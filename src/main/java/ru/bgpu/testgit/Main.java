73dfe03876343ece7e5c1fd45d74ab6ea8f1e30f
out <piligrim.a3@gmail.com>
2025-01-14 12:11:59 +0900

    add: version

--- src/main/java/ru/bgpu/testgit/Main.java
+++ src/main/java/ru/bgpu/testgit/Main.java
@@ -2,7 +2,9 @@ package ru.bgpu.testgit;
 
 public class Main {
 
+    public static final String VERSION = "0.25.01.14";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello from version:"+VERSION+"!");
     }
 }
